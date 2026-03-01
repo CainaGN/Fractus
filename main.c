15def617ba335dbcb3a8ecbdfee462df3fb21214
Cainã Gonçalves Nascimento <cainagnascimento@gmail.com>
2026-03-01 01:02:19 -0400

    Rondomização das branchs

--- main.c
+++ main.c
@@ -11,12 +11,21 @@
 
 #define MAX_DEPTH 10
 
+float rand_float(float x, float percentage)
+{
+
+    float min = percentage * x;
+    float max = x;
+    float random = (float)rand() / RAND_MAX;
+
+    return min + random * (max - min);
+}
 
 float variations_position(float a)
 {
-    float max_varation = a * 0.20;
+    float max_variation = a * 0.20;
     float x = (float)rand()/(float)(RAND_MAX/1);
-    return a + (x * max_varation);
+    return a + (x * max_variation);
 }
 
 void draw_fractal(float x, float y, float lenght, float angle, float thickness, int depth) 
@@ -24,26 +33,32 @@ void draw_fractal(float x, float y, float lenght, float angle, float thickness, 
     if (depth > MAX_DEPTH) return;
     depth++;
     
-    float x_end = x + sinf(angle) * lenght;
-    float y_end = y - cosf(angle) * lenght;
+    //float fator = rand_float(lenght, 0.8);
+
+    
+
+    float x_end = (x + sinf(angle) * rand_float(lenght, 0.7));
+    float y_end = (y - cosf(angle) * rand_float(lenght, 0.6));
     Vector2 end = {x_end, y_end};
     Vector2 start = {x, y};
+
+   // printf("fator: %f\n x: %f\n y: %f\n\n", fator, x_end, y_end);
+
+ 
 
 
     DrawLineEx(start, end, thickness, BLACK);
     float new_lenght = lenght * 0.7;
     float new_right_angle = angle + OFF_ANGLE;
     float new_left_angle = angle - OFF_ANGLE;
-    draw_fractal(x_end, y_end, new_lenght, variations_position(new_right_angle), thickness * 0.7, depth);
-    draw_fractal(x_end, y_end, new_lenght, variations_position(new_left_angle), thickness * 0.7, depth);
-    //draw_fractal(variations_position(end.x), variations_position(end.y), new_lencht, new_angle, thickness * 0.7, depth);
-    
+    draw_fractal(x_end, y_end, rand_float(new_lenght, 0.9), variations_position(new_right_angle), thickness * 0.7, depth);
+    draw_fractal(x_end, y_end, rand_float(new_lenght, 0.9), variations_position(new_left_angle), thickness * 0.7, depth);
 }
 
 void draw_window() 
 {
     InitWindow(WIDTH, HEIGHT, "Fractals");
-    SetTargetFPS(1);
+    SetTargetFPS(2);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -53,8 +68,8 @@ void draw_window()
     CloseWindow();
 }
 
-int main() {
-    printf("Hello, World!\n");
+int main() 
+{
     draw_window();
     return 0;
 }
