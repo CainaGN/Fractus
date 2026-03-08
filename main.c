5b75f3377bad41725127d5912e51d94a4d06f070
Cainã Gonçalves Nascimento <cainagnascimento@gmail.com>
2026-03-08 01:28:14 -0400

    Seed to make sure each iteration will keep the previous branches the same

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 #include <unistd.h> //for sleep() function 
 //#include <time.h> // for seed
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 1024
+#define HEIGHT 720
 
-#define STARTING_THICKNESS 26
+#define STARTING_THICKNESS 30
 #define OFF_ANGLE 20*DEG2RAD
 
 #define MAX_DEPTH 18
-//#define BROWN (Color){ 127, 106, 79, 255 }
-//#define GREEN (Color){ 0, 228, 48, 255 }
+
+#define BACKGROUND (Color){13, 17, 23, 0} // background color of Github
 
 float rand_float(float x, float percentage)
 {
@@ -25,24 +25,32 @@ float rand_float(float x, float percentage)
     return min + random * (max - min);
 }
 
-float variations_position(float a)
+float variate_position(float a)
 {
     float max_variation = a * 0.20;
     float x = (float)rand()/(float)(RAND_MAX/1);
     return a + (x * max_variation);
 }
 
-void draw_fractal(float x, float y, float lenght, float angle, float thickness, int depth, int growing) 
+void draw_fractal(float x, float y, float lenght, float angle, float thickness, int depth, int growing, int seed) 
 {
     if (depth > growing) return;
     depth++;
-    
-    
+
+    srand(84 + seed);
 
     float x_end = (x + sinf(angle) * rand_float(lenght, 0.7));
     float y_end = (y - cosf(angle) * rand_float(lenght, 0.6));
     Vector2 end = {x_end, y_end};
     Vector2 start = {x, y};
+
+    // Here i'll be defining the lenght and angle for the next branches so i wont be losing the Seed
+    // If i calculate this when i call the method, it will go on until the end on the right side and by the time it arrives on the left side, the seed will be lost
+    float new_lenght_right = rand_float((lenght * 0.7), 0.9);
+    float new_lenght_left = rand_float((lenght * 0.7), 0.9);
+    float new_right_angle = variate_position(angle + OFF_ANGLE);
+    float new_left_angle = variate_position(angle - OFF_ANGLE);
+
 
 
     if(depth > 8)
@@ -52,33 +60,30 @@ void draw_fractal(float x, float y, float lenght, float angle, float thickness, 
     {
         DrawLineEx(start, end, thickness, BROWN);
     }
-    float new_lenght = lenght * 0.7;
-    float new_right_angle = angle + OFF_ANGLE;
-    float new_left_angle = angle - OFF_ANGLE;
-    //sleep(1000);
-    draw_fractal(x_end, y_end, rand_float(new_lenght, 0.9), variations_position(new_right_angle), thickness * 0.7, depth, growing);
-    //raw_fractal(x_end, y_end, rand_float(new_lenght, 0.9), angle, thickness * 0.7, depth);
-    draw_fractal(x_end, y_end, rand_float(new_lenght, 0.9), variations_position(new_left_angle), thickness * 0.7, depth, growing);
+    
+ 
+    draw_fractal(x_end, y_end, new_lenght_right, new_right_angle, thickness * 0.7, depth, growing, seed*2);
+    draw_fractal(x_end, y_end, new_lenght_left, new_left_angle, thickness * 0.7, depth, growing, (seed*2)+1);
 }
 
 void draw_window() 
 {
     InitWindow(WIDTH, HEIGHT, "Fractals");
-    SetTargetFPS(4);
+    SetTargetFPS(2);
     int growing = 0;
     
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        srand(12);
+        ClearBackground(BACKGROUND);
         
-        draw_fractal(WIDTH/2, HEIGHT - 20, 170, 0, STARTING_THICKNESS, 0, growing);
+        draw_fractal(WIDTH/2, HEIGHT - 20, 200, 0, STARTING_THICKNESS, 0, growing, 1);
         if(growing <= MAX_DEPTH)
         {
             growing++;
         }
-        
+        TakeScreenshot(TextFormat("screenshots/frame_%02i.png", growing));
+
         EndDrawing();
     }
     CloseWindow();
